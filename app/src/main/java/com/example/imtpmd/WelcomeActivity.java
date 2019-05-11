e53fc391f032c0cda4bed4cbecf35713e28d96e6
LAPTOP-DG0EAET2\Nynke <nynke.vandijk@outlook.com>
2019-05-11 09:58:38 +0200

    Welkom Basic

---
+++ app/src/main/java/com/example/imtpmd/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.imtpmd;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
