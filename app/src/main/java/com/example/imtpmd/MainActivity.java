fd1094f7830d75b7aa1ae03eec23cd1e5cd5be57
LAPTOP-DG0EAET2\Nynke <nynke.vandijk@outlook.com>
2019-05-11 10:14:41 +0200

    First Time Welcome Screen

--- app/src/main/java/com/example/imtpmd/MainActivity.java
+++ app/src/main/java/com/example/imtpmd/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.imtpmd;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        boolean firstStart = prefs.getBoolean("firstStart", true);
+
+        if (firstStart){
+            showStartScreen();
+        }
+    }
+
+    private void showStartScreen() {
+        Intent myIntent = new Intent(getBaseContext(), WelcomeActivity.class);
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("firstStart", false);
+        editor.apply();
+
+        startActivity(myIntent);
     }
 }
