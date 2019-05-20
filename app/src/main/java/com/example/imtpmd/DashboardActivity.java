340ee7fea0938d6846cd7181a9890a98b67dd2ce
Anouk <aananouk@gmail.com>
2019-05-20 19:01:29 +0200

    Bottom navigation bar toegevoegd aan dashboard

--- app/src/main/java/com/example/imtpmd/DashboardActivity.java
+++ app/src/main/java/com/example/imtpmd/DashboardActivity.java
@@ -1,13 +1,63 @@
 package com.example.imtpmd;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private BottomNavigationView DashboardNav;
+    private FrameLayout DashboardFrame;
+
+    private HomeFragment homeFragment;
+    private CalendarFragment calendarFragment;
+    private SettingsFragment settingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        DashboardNav = (BottomNavigationView) findViewById(R.id.dashboard_nav);
+        DashboardFrame = (FrameLayout) findViewById(R.id.dashboard_frame);
+
+        homeFragment = new HomeFragment();
+        calendarFragment = new CalendarFragment();
+        settingsFragment = new SettingsFragment();
+
+        setFragment(homeFragment);
+
+        DashboardNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()){
+                    case R.id.nav_home:
+                        setFragment(homeFragment);
+                        return true;
+
+                    case R.id.nav_calendar:
+                        setFragment(calendarFragment);
+                        return true;
+
+                    case R.id.nav_settings:
+                        setFragment(settingsFragment);
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.dashboard_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
